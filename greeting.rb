6c4dc54dcc215e2388f5d579bb18cccc2df064d4
YUKIKO IKEDA <applelele@gmail.com>
2021-03-15 03:27:05 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,40 @@
+# def greeting
+#   puts "Hello World"
+# end
+# greeting
+# greeting
+# greeting
+# greeting
+# greeting
+
+# def greeting_a_person(name)
+#   puts "Hello #{name}"
+# end
+
+# # greeting_a_person("Maria")
+
+# name = "Maria"
+# greeting_a_person(name)
+
+
+# def greeting_programmer(name, language)
+#   puts "Hello, #{name}. We heard you are a great #{language} programmer."
+# end
+
+# greeting_programmer("Steven", "Elixir")
+
+
+# def greeting(name)
+# puts "Hello, #{name}"
+# end
+# greeting(name)
+
+# def greeting(name)
+#   puts "Hello, #{name}"
+# end
+# greeting("Maria", "Ruby")
+
+def greeting(name = "neighbor")
+  puts "Hello, #{name}"
+end
+greeting("Steven")
---
+++ return.rb
@@ -0,0 +1,15 @@
+# def a_method(a, b)
+#   puts "Hi"
+#   a + b
+# end
+
+# a_method(1, 2)
+
+def a_method(a,b)
+  puts "I got #{a}"
+  puts "I got #{b}"
+  sum = a+b
+  puts "I got #{sum}"
+  sum
+end
+a_method(2,3)
