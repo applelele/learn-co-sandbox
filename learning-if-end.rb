7872802ba2421d82f6b6159c4602509de330ab7e
YUKIKO IKEDA <applelele@gmail.com>
2021-06-13 15:55:06 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,109 @@
+# run_code_inside = false
+# puts "Code before if ... end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+
+# chance_of_rain = 0.2
+# puts "Let's go outside!"
+# if chance_of_rain > 0.5
+#   puts "Pack an umbrella!"
+# else
+#   puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen!"
+
+# chance_of_rain = -23
+# if chance_of_rain <= 0.25
+#   puts "Pack a sun shelter!"
+# elsif (chance_of_rain >0.25 && chance_of_rain <0.75)
+# puts "Pack an umbrella"
+# else
+#   puts "Stay home and read Hegel"
+# end
+
+# puts "You know what yearit is?"
+# this_year=2019
+# puts "It's not 2019!" unless this_year==2019
+
+# this_year = Time.now.year
+# puts "It is #{this_year}"
+
+# current_weather = "raining"
+ 
+# case current_weather
+# when "sunny"
+#   puts "grab some sunscreen!"
+# when "raining"
+#   puts "grab an umbrella"
+# when "snowing"
+#   puts "bundle up"
+# end
+
+# while true do
+#   puts "say this forever..."
+# end
+# while 1 do
+#   puts "say this forever..."
+# end
+
+# count=0
+# while count < 3 do
+#   puts " I am the #{count}, I love to count!"
+#   count += 1
+# end
+
+# magic_exit_number = 7
+# count = 0
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+#   count += 1
+# end
+
+# magic_exit_number = 7
+# count = 0
+# while count < 10 && count != magic_exit_number do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+#   count += 1
+# end
+
+# n = 2
+# count = 0
+# while count <= n do
+#   puts "I ran"
+#   count += 1
+# end
+
+# 3.times do
+#   puts "I run."
+# end
+
+# count = 0
+# n = 3
+# loop do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+# count = 0
+# n = 3
+# while true do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+# counter = 0
+# until counter == 20
+# puts "The current number is less than 20"
+# counter += 1
+# end
+
+def number
+  p 123
+end
+number
